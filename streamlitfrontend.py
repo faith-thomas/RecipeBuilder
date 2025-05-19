c73594f8358bd895458eee4545d38a12d7a608d3
faith-thomas <faiththomas654@gmail.com>
2025-05-19 15:28:56 -0700

    Update streamlitfrontend.py

--- streamlitfrontend.py
+++ streamlitfrontend.py
@@ -3,4 +3,15 @@ import streamlit as st
 st.title("Recipe Builder")
 
 user_input = st.text_input("List your ingredients here")
-st.write("The ingredients you listed are: ", user_input)
+
+
+if user_input:
+    st.write("The ingredients you listed are: ", user_input)
+    
+    st.subheader("Choose your style of recipe!")
+    user_input2 = st.text_input("Pick a style that you want to create with your ingredients")
+    
+    if user_input2:
+        st.write("The style you choose is: ", user_input2)
+    
+
